7b6102611bbed2dc43dac381957f354fdc253c78
Marc-André Appel <marc-andre@appel.fun>
2021-07-07 21:49:14 +0200

    wip: save the project as-is

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    purge: {
+        enabled: true,
+        content: [
+            './*.html',
+        ],
+    },
+    darkMode: 'class',
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
